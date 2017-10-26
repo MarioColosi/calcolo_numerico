dc3a43e9dc3834a3c1d031026cc7cabc38abab7f
Grazia Chillé <grazia020796@gmail.com>
2017-10-26 11:03:17 +0200

    Ho aggiunto confronto norme vettoriali

---
+++ II_Gruppo/2-calcolo_norme_vettoriali_e_grafici/confronto_norme_vett.f
@@ -0,0 +1,68 @@
+	PROGRAM CONFRONTO_NORME
+* Considerare vettori con un numero n di componenti da 2 a 15, 
+* le cui componenti sono punti equidistanti dell'intervallo [-1,1]. 
+* Calcolare le tre norme vettoriali al crescere di n e farne il confronto
+* analizzando il grafico dei risultati.
+
+	PARAMETER(MAX_N=15)
+	REAL VETT(MAX_N)
+	REAL NORMA(MAX_N-1)
+	REAL NORMA1,NORMA2,NORMAINF
+
+	DO N=2,MAX_N
+		CALL INIT_VETT(VETT,N)
+		NORMA(N-1)=NORMA1(VETT,N)
+NORMA(N-1)=NORMA2(VETT,N)
+NORMA(N-1)=NORMAINF(VETT,N)
+	END DO
+	CALL WRITE_FILE(NORMA,MAX_N-1)
+	END
+
+      SUBROUTINE INIT_VETT(VETT,N)
+	PARAMETER(K=-1,L=1)
+	REAL VETT(N)
+	REAL DISTANZA
+	DISTANZA=FLOAT((ABS(K)+ABS(L)))/N
+	VETT(1)=K
+	DO I=2,N-1
+		VETT(I)=VETT(I-1)+DISTANZA
+	END DO
+	VETT(N)=L
+	END	
+		
+	REAL FUNCTION NORMA1(X,N)
+ 	REAL X(N)
+	NORMA1=0
+	DO I=1,N
+		NORMA1=NORMA1+ABS(X(I))
+	END DO
+	END
+
+	REAL FUNCTION NORMA2(X,N)
+	REAL X(N)
+	NORMA2=0
+	DO I=1,N
+		NORMA2=NORMA2+(X(I)*X(I))
+	END DO
+	NORMA2=SQRT(NORMA2)
+	END 
+
+	REAL FUNCTION NORMAINF(X,N)
+	REAL X(N)
+	NORMAINF=X(1)
+	DO I=2,N
+		IF(NORMAINF.LT.X(I))THEN
+			NORMAINF=X(I)
+	    END IF
+	END DO
+	END 
+
+	SUBROUTINE WRITE_FILE(NORMA,N)
+	REAL NORMA(N)
+	DO I=1,N
+		WRITE(3,*)NORMA(I)
+	END DO
+	END
+	
+
+
