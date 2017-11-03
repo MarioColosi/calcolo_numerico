998384795526d5568eb9f3814b52fa1d8134a671
Mario Colosi <colosimario96@gmail.com>
2017-11-03 10:54:37 +0100

    I_Gruppo - Esercizio 2 (EQ2)

---
+++ I_Gruppo/2-eq2/eq2.f
@@ -0,0 +1,28 @@
+      PROGRAM EQ2
+      WRITE(*,*)'*** EQUAZIONI DI SECONDO GRADO: ax^2 + bx + c = 0'
+      WRITE(*,*)'Inserisci il valore di A'
+      READ(*,*)A
+      WRITE(*,*)'Inserisci il valore di B'
+      READ(*,*)B
+      WRITE(*,*)'Inserisci il valore di C'
+      READ(*,*)C
+      CALL CALC_EQ2(A,B,C,X1,X2,X)
+      WRITE(*,*)'La soluzione dell''equazione e'':'
+      WRITE(*,*)'X1 = ',X1
+      WRITE(*,*)'X2 = ',X2
+      WRITE(*,*)'X = ',X
+      END
+
+      SUBROUTINE CALC_EQ2(A,B,C,X1,X2,X)
+      REAL A,B,C,X1,X2,X,DELTA
+      DELTA = B*B-4*A*C
+      WRITE(*,*)'DELTA = ',DELTA
+      IF(DELTA.GT.0)THEN
+        X1 = (-B-SQRT(DELTA))/(2*A)
+        X2 = (-B+SQRT(DELTA))/(2*A)
+      ELSE IF(DELTA.EQ.0)THEN
+        X = -B/2*A
+      ELSE
+        WRITE(*,*)'La soluzione dell''equazione e'' complessa'
+      ENDIF
+      END
