778acc31933587c9b73e5952339e50e94095b1cc
Mario Colosi <colosimario96@gmail.com>
2017-11-03 11:10:21 +0100

    I_Gruppo - Esercizio 3 (Produttoria)

---
+++ I_Gruppo/3-sommatoria_e_produttoria/produttoria.f
@@ -0,0 +1,29 @@
+      PROGRAM PRODUTTORIA
+      PARAMETER(MAX_N=100)
+      REAL X(MAX_N)
+      WRITE(*,*)'*** PRODUTTORIA DI N NUMERI INTERI ***'
+1     FORMAT('Quanti numeri vuoi moltiplicare? [MAX:',I3,']')
+      WRITE(*,1)MAX_N
+      READ(*,*)N
+      WRITE(*,*)
+      IF(N.GT.MAX_N)THEN
+          WRITE(*,*)'ERRORE'
+          STOP
+      END IF
+      CALL INIT_X(X,N)
+      CALL MUL(X,N,RESULT)
+      WRITE(*,*)'La produttoria dei numeri e''',RESULT
+      END
+
+      SUBROUTINE INIT_X(X,N)
+      REAL X(N)
+      READ(*,*)(X(I),I=1,N)
+      END
+
+      SUBROUTINE MUL(X,N,RESULT)
+      REAL X(N)
+      RESULT = 1
+      DO I=1,N
+        RESULT = RESULT*X(I)
+      END DO
+      END
