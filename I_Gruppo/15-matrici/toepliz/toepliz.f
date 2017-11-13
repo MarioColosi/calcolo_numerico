9af769f162babd95aaa3c1c6de29d610883a21d9
Mario Colosi <colosimario96@gmail.com>
2017-11-13 13:26:15 +0100

    I_Gruppo - 15-matrici: Modifiche matrici

--- I_Gruppo/15-matrici/toepliz/toepliz.f
+++ I_Gruppo/15-matrici/toepliz/toepliz.f
@@ -2,36 +2,27 @@
 
 	PARAMETER(MAX_N=500)
 	REAL T(MAX_N,MAX_N)
-	REAL VETT(MAX_N)
 	WRITE(*,*)'Inserisci la grandezza della matrice: '
 	READ(*,*)N
-	CALL INIT_VETT(VETT,N)
-	CALL MATRIX_T(VETT,T,MAX_N,N)
+	CALL MATRIX_T(T,MAX_N,N)
 	DO I=1,N
-	WRITE(*,*)(T(I,J),J=1,N)
-	END DO 
+		WRITE(*,*)(T(I,J),J=1,N)
+	END DO
 	END
 
-	
-      SUBROUTINE INIT_VETT(VETT,N)
-	REAL VETT(-(N-1):N-1)
-	WRITE(*,*)'Inserisci i valori del vettore: '
-	READ(*,*)(VETT(I),I=0,N-1)
-	END
-
-	SUBROUTINE MATRIX_T(VETT,T,MAX_N,N)
-      REAL T(MAX_N,N)
-	REAL VETT(-(N-1):N-1)
+	SUBROUTINE MATRIX_T(T,MAX_N,N)
+	REAL T(MAX_N,N)
+	REAL VETT(-N+1:N-1)
+	CALL INIT_VETT(VETT,N)
 	DO I=1,N
 		DO J=1,N
-			T(I,J)=VETT(J-1)
+			T(I,J)=VETT(I-J)
 		END	DO
 	END DO
 	END
 
-	
-
-	
-
-
-
+	SUBROUTINE INIT_VETT(VETT,N)
+	REAL VETT(-N+1:N-1)
+	WRITE(*,*)'Inserisci i valori del vettore: '
+	READ(*,*)(VETT(I),I=-N+1,N-1)
+	END
