fae6d55c136e0b117aaa40566934775a39ed9149
Mario Colosi <colosimario96@gmail.com>
2017-12-09 11:04:52 +0100

    Commenti I_Gruppo: esercizi 1-4

--- I_Gruppo/3-sommatoria_e_produttoria/produttoria.f
+++ I_Gruppo/3-sommatoria_e_produttoria/produttoria.f
@@ -1,25 +1,36 @@
       PROGRAM PRODUTTORIA
+* 3. Scrivere i programmi per la produttoria di n numeri reali e complessi.
       PARAMETER(MAX_N=100)
       REAL X(MAX_N)
-      WRITE(*,*)'*** PRODUTTORIA DI N NUMERI INTERI ***'
-1     FORMAT('Quanti numeri vuoi moltiplicare? [MAX:',I3,']')
-      WRITE(*,1)MAX_N
+
+*    Sottotitolo (INPUT: 1 stringa da 50 caratteri)
+2	FORMAT(/,2X,52('-'),/,2X,'  ',A,/,2X,52('-'),/)
+*    Messaggio di input (INPUT: 1 stringa, 1 intero)
+3	FORMAT(2X,'[INPUT] ',A,'[MAX = ',I3,']: ',$)
+
+	WRITE(*,2)'         PRODUTTORIA DI N NUMERI INTERI           '
+      WRITE(*,3)'Quanti numeri vuoi moltiplicare? ',MAX_N
       READ(*,*)N
       WRITE(*,*)
       IF(N.GT.MAX_N)THEN
-          WRITE(*,*)'ERRORE'
+          WRITE(*,*)' [ERRORE] Hai inserito un numero troppo alto!'
           STOP
       END IF
       CALL INIT_X(X,N)
       CALL MUL(X,N,RESULT)
-      WRITE(*,*)'La produttoria dei numeri e''',RESULT
+      WRITE(*,*)' [RISULTATO] La produttoria dei numeri e''',RESULT
+	WRITE(*,*)
       END
 
+****** Subroutine di inizializzazione del vettore contenente gli elementi da moltiplicare
       SUBROUTINE INIT_X(X,N)
       REAL X(N)
+ 3	FORMAT(2X,'[INPUT] ',A,'[N =',I3,']: ',$)
+	WRITE(*,3)'Inserisci i numeri da moltiplicare',N
       READ(*,*)(X(I),I=1,N)
       END
 
+****** Subroutine della moltiplicazione degli elementi di un vettore
       SUBROUTINE MUL(X,N,RESULT)
       REAL X(N)
       RESULT = 1
--- I_Gruppo/4-media/media.f
+++ I_Gruppo/4-media/media.f
@@ -1,25 +1,35 @@
       PROGRAM CALCOLAMEDIA
+* 4 - Scrivere un programma che calcola la media di n numeri interi (o reali). Trasformare il programma in una funzione.
       PARAMETER(MAX_N=100)
       REAL X(MAX_N),MEDIA
-      WRITE(*,*)'*** MEDIA DI N NUMERI INTERI ***'
-  1   FORMAT('Di quanti numeri vuoi fare la media? [MAX:',I3,']')
-      WRITE(*,1)MAX_N
+
+*    Sottotitolo (INPUT: 1 stringa da 50 caratteri)
+2	FORMAT(/,2X,52('-'),/,2X,'  ',A,/,2X,52('-'),/)
+*    Messaggio di input (INPUT: 1 stringa, 1 intero)
+3	FORMAT(2X,'[INPUT] ',A,'[MAX = ',I3,']: ',$)
+																	
+      WRITE(*,2)'            MEDIA DI N NUMERI INTERI              '
+	WRITE(*,3)'Di quanti numeri vuoi fare la media? ',MAX_N
       READ(*,*)N
-      WRITE(*,*)
       IF(N.GT.MAX_N)THEN
-          WRITE(*,*)'ERRORE'
+          WRITE(*,*)' [ERRORE] Hai inserito un numero troppo alto!'
           STOP
       END IF
       CALL INIT_X(X,N)
       RESULT = MEDIA(X,N)
-      WRITE(*,*)'La media e''',RESULT
+      WRITE(*,*)' [RISULTATO] La media dei numeri e''',RESULT
+	WRITE(*,*)
       END
 
+****** Subroutine di inizializzazione del vettore contenente gli elementi di cui fare la media
       SUBROUTINE INIT_X(X,N)
       REAL X(N)
+ 3	FORMAT(2X,'[INPUT] ',A,'[N =',I3,']: ',$)
+	WRITE(*,3)'Inserisci i numeri di cui vuoi fare la media',N
       READ(*,*)(X(I),I=1,N)
       END
 
+****** Funzione di calcolo della media
       REAL FUNCTION MEDIA(X,N)
       REAL X(N)
       MEDIA = 0.0
